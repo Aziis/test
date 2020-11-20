0aba4148d29c373ff063291950f14e3d0934e36a
Auxane <sansaeva590@gmail.com>
2020-11-20 15:34:39 +0100

    Ajout de la Javadoc pour SuperClasse

--- src/unPackage/SuperClasse.java
+++ src/unPackage/SuperClasse.java
@@ -1,12 +1,23 @@
 package unPackage;
 
 public class SuperClasse {
+    /**
+     * String nom
+     */
     private String nom;
 
+    /**
+     * Constructeur de la classe SuperClasse
+     * @param nom String
+     */
     public SuperClasse(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Fonction qui retourne un mot
+     * @return String
+     */
     public String getNom() {
         return nom;
     }
