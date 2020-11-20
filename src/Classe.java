cc2fcc6a61fb0fd364b2040f7096267ac1480447
Auxane <sansaeva590@gmail.com>
2020-11-20 15:31:39 +0100

    Ajout de la SuperClasse

--- src/Classe.java
+++ src/Classe.java
@@ -1,5 +1,8 @@
+import unPackage.SuperClasse;
 public class Classe {
     public static void main(String[] args){
         System.out.println("Test");
+        SuperClasse sc = new SuperClasse("Hey");
+        System.out.println(sc.getNom());
     }
 }
---
+++ src/unPackage/SuperClasse.java
@@ -0,0 +1,13 @@
+package unPackage;
+
+public class SuperClasse {
+    private String nom;
+
+    public SuperClasse(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
